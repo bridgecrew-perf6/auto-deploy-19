d6f118dd0c15773f1850bf956784eac728b380f2
Code Like Pro <ma@MAs-MacBook-Pro.local>
2022-01-31 15:32:49 +0100

    test change

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,7 +16,7 @@ function App() {
             />
             <h2>Full Stack Developer</h2>
             <p>
-              Education: <strong>Code Like Pro2</strong>
+              Education: <strong>Code Like Pro3</strong>
             </p>
             <p>
               Work: <b>Google</b>
